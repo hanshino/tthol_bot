d6f9295e164b5f086f2b1d8f81597a2de5bcd5b2
cameron.hsieh <cameron.hsieh@coolbe.com.tw>
2021-05-06 16:20:45 +0800

    測試環境調整

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 "use strict";
+const isDEV = process.env.NODE_ENV !== "production";
 
-if (process.env.NODE_ENV !== "production") {
+if (isDEV) {
   require("dotenv").config();
 }
 
@@ -11,7 +12,19 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  const embed = new Discord.MessageEmbed();
+
+  embed
+    .setTitle("小幫手啟動")
+    .setColor("#AB1234")
+    .setDescription("開始服務！")
+    .setFooter(new Date().toLocaleTimeString());
+
+  if (!isDEV) {
+    client.channels.fetch("839773570359099434").then(channel => channel.send(embed));
+  } else {
+    console.log("小幫手啟動囉！");
+  }
 });
 
 client.on("message", MessageHandle);
