92259e67a9b8abd848e130142dd58ddba9d95ae0
hanshino <c0952199517@gmail.com>
2023-03-20 02:32:33 +0800

    新增狀態訊息

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-import { Client, GatewayIntentBits, Collection } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  Collection,
+  ActivityType,
+} from "discord.js";
 import { Command, MyClient } from "./type";
 import { Sequelize } from "sequelize";
 import path from "node:path";
@@ -8,6 +13,14 @@ const { Guilds, MessageContent, GuildMessages, GuildMembers } =
 
 const client = new Client({
   intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
+  presence: {
+    activities: [
+      {
+        name: "tthol.bot | 輸入 (/) 觀看指令",
+        type: ActivityType.Playing,
+      },
+    ],
+  },
 }) as MyClient;
 
 client.commands = new Collection<string, Command>();
